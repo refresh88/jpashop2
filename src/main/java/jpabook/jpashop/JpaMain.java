ed6ed7b5ad5055e25710c91cb3178425a98e4d1e
refresh88 <ouok88@gmail.com>
2023-07-27 23:42:24 +0900

    section4 order class add member

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,6 +18,17 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            // 주문한 Member를 찾고 싶을때 아래와 같은 과정을 거쳐야하는데,
+            // 이상함. 객체지향적이지 않음.
+            // 따라서, Order 객체에 Member를 리턴하는 요소가 필요해 보임.
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
+            Member member = em.find(Member.class, memberId);
+            
+            // 이런식으로 바로 꺼낼수있어야 함.
+            Member findMember = order.getMember();
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,6 +16,16 @@ public class Order {
     @Column(name = "MEMBER_ID")
     private Long memberId;
 
+    private Member member;
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
